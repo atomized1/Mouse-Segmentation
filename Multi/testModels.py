b18eb1d1cb8b7990e57ac193f424bd59b7fbd0f3
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-05-19 18:29:12 -0400

    Update testModels.py

--- Multi/testModels.py
+++ Multi/testModels.py
@@ -241,6 +241,7 @@ def deconvertTruth(labels):
 
 def imageGen(labels):
     plt.figure(1)
+    labels = np.ma.masked_where(labels != 91, labels)
     plt.imshow(labels[130, :, :, 0])
     plt.savefig('visuals.png')
 

b698d22296a16e902e2547a411d20beb3d20742c
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-05-19 16:12:40 -0400

    Create testModels.py

---
+++ Multi/testModels.py
@@ -0,0 +1,216 @@
+import tensorflow as tf
+import nibabel as nib
+import keras
+import keyboard
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+dirnam = os.path.dirname(__file__)
+
+
+def getData():
+    # A fuction that loads in the data from a list of files
+    # Inputs: none
+    # Outputs: Arrays containing slices of each image
+
+    #variables used to divide up data
+    alt = 0
+    countA = 0
+    countB = 0
+
+    #The empty arrays that will store filenames for each set
+    imageListTrain = []
+    imageListPredict = []
+    maskListTrain = []
+    maskListPredict = []
+
+    #A short loop to split all the data into one of the 4 lists.
+    with open(os.path.normpath(os.path.join(dirnam, '2.txt'))) as files:
+        for arrayDataPath in files:
+            if alt == 0:
+                if countA <= 6:
+                    imageListTrain.append(arrayDataPath.strip())
+                    countA += 1
+                else:
+                    imageListPredict.append(arrayDataPath.strip())
+                    countA += 1
+                    if countA == 10:
+                        countA = 0
+                alt = 1
+            elif alt == 1:
+                if countB <= 6:
+                    maskListTrain.append(arrayDataPath.strip())
+                    countB += 1
+                else:
+                    maskListPredict.append(arrayDataPath.strip())
+                    countB += 1
+                    if countB == 10:
+                        countB = 0
+                alt = 0
+
+    #Loading in all the data from the filenames using the initialize function defined below
+    #arrayData, arrayTruth = initialize(imageListTrain, maskListTrain)
+    arrayPredictData, arrayPredictTruth = initialize(imageListPredict, maskListPredict)
+
+    return arrayPredictData, arrayPredictTruth
+
+
+def normalize(data):
+    #initializing some variables so I can calculate mean intensity
+    total = 0
+    count = 0
+    #A loop to calculate that mean
+    for x in range(0, len(data)):
+        for y in range(0, len(data[1])):
+            for z in range(0, len(data[1][1])):
+                total += data[x][y][z]
+                count += 1
+    mean = total / count
+
+    #Using that mean to calculate variance, and then Standard deviation
+    totalVariance = 0
+    for x in range(0, len(data)):
+        for y in range(0, len(data[1])):
+            for z in range(0, len(data[1][1])):
+                variance = pow(data[x][y][z] - mean, 2)
+                totalVariance += variance
+    Std = totalVariance / count
+
+    #Adjusting all values appropriately
+    for x in range(0, len(data)):
+        for y in range(0, len(data[1])):
+            for z in range(0, len(data[1][1])):
+                data[x][y][z] -= mean
+                data[x][y][z] /= Std
+
+    return data
+
+
+def initialize(imageList, maskList):
+    #Counting how many slices we need to load in, then preallocating arrays of that size
+    slices = 0
+    print(os.path.join(dirnam, imageList[0]))
+    for x in range(0, len(imageList)):
+        image = nib.load(os.path.normpath(os.path.join(dirnam, imageList[x])))
+        data = image.get_fdata()
+        slices = slices + len(data[0])
+        print(slices)
+
+    arrayData = np.empty([slices, 1, 148, 100])
+    arrayTruth = np.empty([slices, 1, 148, 100])
+
+    #Loading in each slice.  The start value offsets by the total amount of slices loaded, so not slice is overridden
+    start = 0
+    for file in range(0, len(imageList)):
+        image = nib.load(os.path.normpath(os.path.join(dirnam, imageList[file])))
+        data = image.get_fdata()
+        data = normalize(data) #Adjusting the data using mean and std
+        mask = nib.load(os.path.normpath(os.path.join(dirnam, maskList[file])))
+        truth = mask.get_fdata()
+        data = np.rot90(data, axes=(0, 1))  #Rotating data so the thickest side is in the 0 index
+        truth = np.rot90(truth, axes=(0, 1))
+        print(imageList[file])
+        for x in range(0, len(data)):
+            arrayData[x + start, 0] = data[x, 0:148]
+            arrayTruth[x + start, 0] = truth[x, 0:148]
+        start = start + len(data)
+        print("File Loaded")
+
+    return arrayData, arrayTruth
+
+
+def display(data, model):
+
+    #Saving the model, and generating the visual to insure it worked
+    model.save('myModel')
+    predictions = model.predict(data[50:51])
+    predictions = np.round(predictions)
+
+    plt.ion()
+    plt.axis('off')
+    plt.figure(4)
+
+    print(len(predictions[0][0][0]))
+
+    plt.imshow(data[50, :, :, 0], cmap='gray')
+    plt.imshow(predictions[0, :, :, 0], alpha=0.2)
+    plt.draw()
+    plt.pause(0.01)
+
+    x = 0
+    View = 0
+
+    while True:
+        if View == 0:
+            if x > len(data[50, 0, 0]) - 1:
+                x = len(data[50, 0, 0]) - 1
+            plt.clf()
+            plt.imshow(data[50, :, :, x], cmap='gray')
+            plt.imshow(predictions[0, :, :, x], alpha=0.2)
+            plt.axis('off')
+            plt.draw()
+            plt.pause(0.01)
+        if View == 1:
+            if x > len(data[3][0]) - 1:
+                x = len(predictions[0][0]) - 1
+            plt.clf()
+            plt.imshow(data[50, :, x, :], cmap='gray')
+            plt.imshow(predictions[0, :, x, :], alpha=0.2)
+            plt.axis('off')
+            plt.draw()
+            plt.pause(0.01)
+        if View == 2:
+            if x > len(data[0]) - 1:
+                x = len(predictions[0]) - 1
+            plt.clf()
+            plt.imshow(data[50, x, :, :], cmap='gray')
+            plt.imshow(predictions[0, x, :, :], alpha=0.2)
+            plt.axis('off')
+            plt.draw()
+            plt.pause(0.01)
+
+        if keyboard.read_key() == "t":
+            x += 1
+            print(x)
+        elif keyboard.read_key() == "g":
+            x -= 1
+            print(x)
+        elif keyboard.read_key() == "w":
+            View = 0
+        elif keyboard.read_key() == "e":
+            View = 1
+        elif keyboard.read_key() == "r":
+            View = 2
+        elif keyboard.read_key() == "y":
+            break
+        if x < 0:
+            x = 0
+
+
+def main():
+    model = keras.models.load_model('modelsGlobal\Model')
+    arrayData, layerTruth = getData()
+    arrayData = np.rot90(arrayData, axes=(1, 3))
+    layerTruth = np.rot90(layerTruth, axes=(1, 3))
+    history = model.evaluate(arrayData, layerTruth)
+
+    xvals = np.arange(len(arrayData))
+    plt.figure(1)
+    plt.plot(xvals, history.history['binary_accuracy'])
+    plt.savefig('plotAccuracy.png')
+    np.savetxt(os.path.join(dirnam, "Accuracy.csv"), history.history['binary_accuracy'], delimiter=",")
+
+    plt.figure(2)
+    plt.plot(xvals, history.history['loss'])
+    plt.savefig('plotLoss.png')
+    np.savetxt(os.path.join(dirnam, "Loss.csv"), history.history['loss'], delimiter=",")
+
+    plt.figure(3)
+    plt.plot(xvals, history.history['dice_metric'])
+    plt.savefig('plotDice.png')
+    np.savetxt(os.path.join(dirnam, "Dice.csv"), history.history['dice_metric'], delimiter=",")
+
+
+if __name__ == "__main__":
+    main()

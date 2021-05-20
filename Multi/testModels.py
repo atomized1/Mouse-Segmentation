37f5c0032dccbeb14ff327371cd11c4ca595b228
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-05-20 18:46:20 -0400

    Update testModels.py

--- Multi/testModels.py
+++ Multi/testModels.py
@@ -227,7 +227,7 @@ def convertTruth(mask):
 
 def deconvertTruth(labels):
     newTruth = np.empty((len(labels), len(labels[0]), len(labels[0,0])), dtype=np.dtype('int32'))
-    for x in range(0, 100):
+    for x in range(0, 180):
         print(x)
         for y in range(0, len(labels[0])):
             for z in range(0, len(labels[0, 0])):

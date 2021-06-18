0c2aefcd865b918fefdf9592092f16b232f1a725
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-06-18 15:47:09 -0400

    Update Display.py

--- Multi/Display.py
+++ Multi/Display.py
@@ -170,7 +170,10 @@ def total_dice(y_true, y_pred):
 
 def dice_metric_label(y_true, y_pred, label):
     y_predFiltered = y_pred == label
-    y_trueFiltered = y_true == label + 1000 - 165
+    if label < 166:
+        y_trueFiltered = y_true == label
+    else:
+        y_trueFiltered = y_true == label + 1000 - 165
     y_trueReshaped = np.zeros([len(y_true), len(y_true[0]), len(y_true[0, 0])])
     for x in range(0, len(y_true)):
         for y in range(0, len(y_true[0])):

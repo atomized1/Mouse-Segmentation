f7ba21c2c4d6ff7c488dc33f6534c76cc0e413a4
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-09-24 00:31:34 -0400

    Update 3DModel.py

--- Multi/3DModel.py
+++ Multi/3DModel.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 import nibabel as nib
 import keras
-import keyboard
 import numpy as np
-import matplotlib.pyplot as plt
 import os
-import sys
 
 dirnam = os.path.dirname(__file__)
+epochs = 20
+BURST = 10
 
 def dice_metric(y_true, y_pred):
 
@@ -30,38 +29,6 @@ def dice_metric(y_true, y_pred):
     #hard_dice = tf.reduce_mean(hard_dice)
 
     return hard_dice
-
-
-def dice_metric_label(y_true, y_pred, label):
-    y_predFiltered = y_pred == label
-    if label < 166:
-        y_trueFiltered = y_true == label
-    else:
-        y_trueFiltered = y_true == label + 1000 - 165
-    y_trueReshaped = np.zeros([len(y_true), len(y_true[0]), len(y_true[0, 0])])
-    for x in range(0, len(y_true)):
-        for y in range(0, len(y_true[0])):
-            for z in range(0, len(y_true[0, 0])):
-                y_trueReshaped[x, y, z] = y_trueFiltered[x, y, z, 0]
-
-    y_predFiltered = y_predFiltered.astype(int)
-    y_trueReshaped = y_trueReshaped.astype(int)
-    intersect = y_predFiltered & y_trueReshaped
-    intersect = intersect.astype(int)
-    intersect = intersect.sum()
-    y_predFiltered = y_predFiltered.sum()
-    y_trueReshaped = y_trueReshaped.sum()
-
-    print(y_trueReshaped, y_predFiltered)
-
-    hard_dice = 2 * intersect / (y_predFiltered + y_trueReshaped)
-
-    f = open("DiceScores.txt", "a")
-    f.write(str(label) + " " + str(hard_dice) + "\n")
-    f.close()
-
-    print(label, hard_dice)
-    return y_predFiltered, y_trueReshaped
 
 
 def getData():
@@ -106,9 +73,9 @@ def getData():
 
     #Loading in all the data from the filenames using the initialize function defined below
     arrayData, arrayTruth = initialize(imageListTrain, maskListTrain)
-    arrayPredictData, arrayPredictTruth = initialize(imageListPredict, maskListPredict)
+    #arrayPredictData, arrayPredictTruth = initialize(imageListPredict, maskListPredict)
 
-    return arrayTruth, arrayPredictData, arrayPredictTruth
+    return arrayData, arrayTruth, #arrayPredictData, arrayPredictTruth
 
 
 def normalize(data):
@@ -190,72 +157,17 @@ def initialize(imageList, maskList):
     return arrayData, arrayTruth
 
 
-def display(data, model):
+def multichannel(data):
+    arrayData = np.empty((len(data), len(data[0]), len(data[0, 0]), 3))
+    for x in range(1, len(data)-1):
+        for y in range(0, len(data[0])):
+            for z in range(0, len(data[0, 0])):
+                arrayData[x, y, z, 0] = data[x-1, y, z]
+                arrayData[x, y, z, 1] = data[x, y, z]
+                arrayData[x, y, z, 2] = data[x+1, y, z]
 
-    #Saving the model, and generating the visual to insure it worked
-    model.save('myModel')
-    predictions = model.predict(data[50:51])
-    predictions = np.round(predictions)
+    return arrayData
 
-    plt.ion()
-    plt.axis('off')
-    plt.figure(4)
-
-    print(len(predictions[0][0][0]))
-
-    plt.imshow(data[50, :, :, 0], cmap='gray')
-    plt.imshow(predictions[0, :, :, 0], alpha=0.2)
-    plt.draw()
-    plt.pause(0.01)
-
-    x = 0
-    View = 0
-
-    while True:
-        if View == 0:
-            if x > len(data[50, 0, 0]) - 1:
-                x = len(data[50, 0, 0]) - 1
-            plt.clf()
-            plt.imshow(data[50, :, :, x], cmap='gray')
-            plt.imshow(predictions[0, :, :, x], alpha=0.2)
-            plt.axis('off')
-            plt.draw()
-            plt.pause(0.01)
-        if View == 1:
-            if x > len(data[3][0]) - 1:
-                x = len(predictions[0][0]) - 1
-            plt.clf()
-            plt.imshow(data[50, :, x, :], cmap='gray')
-            plt.imshow(predictions[0, :, x, :], alpha=0.2)
-            plt.axis('off')
-            plt.draw()
-            plt.pause(0.01)
-        if View == 2:
-            if x > len(data[0]) - 1:
-                x = len(predictions[0]) - 1
-            plt.clf()
-            plt.imshow(data[50, x, :, :], cmap='gray')
-            plt.imshow(predictions[0, x, :, :], alpha=0.2)
-            plt.axis('off')
-            plt.draw()
-            plt.pause(0.01)
-
-        if keyboard.read_key() == "t":
-            x += 1
-            print(x)
-        elif keyboard.read_key() == "g":
-            x -= 1
-            print(x)
-        elif keyboard.read_key() == "w":
-            View = 0
-        elif keyboard.read_key() == "e":
-            View = 1
-        elif keyboard.read_key() == "r":
-            View = 2
-        elif keyboard.read_key() == "y":
-            break
-        if x < 0:
-            x = 0
 
 
 def convertTruth(mask):
@@ -280,81 +192,56 @@ def convertTruth(mask):
                             154 >= mask[x, y, z, a] >= 152) or (160 >= mask[x, y, z, a] >= 158) or mask[x, y, z, a] == 162 or mask[x, y, z, a] == 164 or mask[x, y, z, a] == 166:
                         new[2] = 1
                         newTruth[x, y, z, a] = new
-                    else:
-                        print(mask[x,y,z,a])
-        return newTruth
-
-
-def deconvertTruth(labels):
-    newTruth = np.empty((len(labels), len(labels[0]), len(labels[0,0]), len(labels[0,0,0])), dtype=np.dtype('int32'))
-    for x in range(0, len(labels)):
-        print(x)
-        for y in range(0, len(labels[0])):
-            for z in range(0, len(labels[0, 0])):
-                for b in range(0, len(labels[0,0,0])):
-                    biggestNum = 0
-                    biggestLabel = 0
-                    #print(labels[x,y,z,b])
-                    for a in range(0, int(sys.argv[2])):
-                        if biggestNum < labels[x,y,z,b,a]:
-                            biggestNum = labels[x,y,z,b,a]
-                            biggestLabel = a
-                    newTruth[x,y,z,b] = biggestLabel
     return newTruth
 
 
-def imageGen(labels):
-    plt.figure(1)
-    #print(labels[70, 60, 60])
-    labels = labels.astype('int32')
-    #plt.imshow(labels[70, :, :])
-    plt.savefig('visuals.png')
-
-    for x in range(0, len(labels)):
-        img = nib.Nifti1Image(labels[x], np.eye(4))
-        nib.save(img, 'results' + str(sys.argv[1]) + str(x) + '.nii.gz')
-
-
-def multichannel(data):
-    arrayData = np.empty((len(data), len(data[0]), len(data[0, 0]), 3))
-    for x in range(1, len(data)-1):
-        for y in range(0, len(data[0])):
-            for z in range(0, len(data[0, 0])):
-                arrayData[x, y, z, 0] = data[x-1, y, z]
-                arrayData[x, y, z, 1] = data[x, y, z]
-                arrayData[x, y, z, 2] = data[x+1, y, z]
-
-    return arrayData
-
-
-def sortLabels(data):
-    totals = np.empty(3)
-    for x in range(0, 3):
-        totals[x] = np.sum(data == x)
-    sortedIndex = np.argsort(totals)
-    return sortedIndex
-
-
 def main():
-    trainingArrayData, arrayData, layerTruth = getData()
+    input_layer = keras.layers.Input(shape=(100, 200, 180, 1))
+    conv1a = keras.layers.Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same')(input_layer)
+    conv1b = keras.layers.Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same')(conv1a)
+    pool1 = keras.layers.MaxPool3D(pool_size=(2, 2, 2))(conv1b)
+    conv2a = keras.layers.Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(pool1)
+    conv2b = keras.layers.Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(conv2a)
+    pool2 = keras.layers.MaxPool3D(pool_size=(2, 2, 2))(conv2b)
+    conv3a = keras.layers.Conv3D(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(pool2)
+    conv3b = keras.layers.Conv3D(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(conv3a)
+
+    dconv3a = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(conv3b)
+    dconv3b = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(dconv3a)
+    unpool2 = keras.layers.UpSampling3D(size=(2, 2, 2))(dconv3b)
+    cat2 = keras.layers.concatenate([conv2b, unpool2])
+    dconv2a = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(cat2)
+    dconv2b = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(dconv2a)
+    unpool1 = keras.layers.UpSampling3D(size=(2, 2, 2))(dconv2b)
+    cat1 = keras.layers.concatenate([conv1b, unpool1])
+    dconv1a = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(cat1)
+    dconv1b = keras.layers.Conv3DTranspose(filters=96, kernel_size=(3, 3, 3), activation='relu', padding='same')(dconv1a)
+
+    output = keras.layers.Conv3D(filters=3, kernel_size=(3, 3, 3), activation='softmax', padding='same')(dconv1b)
+
+    model = keras.models.Model(input_layer, output)
+    opt = keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=[dice_metric])
+
+    arrayData, layerTruth = getData()
     arrayData = np.rot90(arrayData, axes=(1, 4))
     layerTruth = np.rot90(layerTruth, axes=(1, 4))
-    layerTruth = convertTruth(layerTruth)
 
-    model = keras.models.load_model(sys.argv[1] + '/Model', custom_objects={"dice_metric": dice_metric})
-    opt = keras.optimizers.Adam()
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=[keras.metrics.binary_accuracy, dice_metric])
-    model.evaluate(arrayData, layerTruth, batch_size=1)
-    history = model.predict(arrayData, batch_size=1)
+    print(len(layerTruth))
+    print(len(layerTruth[0]))
+    print(len(layerTruth[0, 0]))
+    print(len(layerTruth[0, 0, 0]))
 
-    totalImage = deconvertTruth(history)
-    #print(history.shape)
-    #print(layerTruth.shape)
-    #print(totalImage.shape)
-    dice_metric_label(layerTruth[1], totalImage[1], 1)
+    print(len(arrayData))
 
-    imageGen(totalImage)
+
+    arrayTruth = convertTruth(layerTruth)
+    #arrayDataMult = multichannel(arrayData)
+    history = model.fit(arrayData, arrayTruth, epochs=epochs, batch_size=1)
+
+    model.save(os.path.join(dirnam, "modelsOf5/Model"))
 
 
 if __name__ == "__main__":
     main()
+
